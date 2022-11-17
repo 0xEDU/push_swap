12df2e9ec35f935b84012a7e7279e7bdfd0f40ec
Eduardo Tachotte <etachott@c1r7p3.42sp.org.br>
2022-11-17 16:21:00 -0300

    Finished validations

--- sources/validate_input.c
+++ sources/validate_input.c
@@ -6,11 +6,29 @@
 /*   By: etachott <etachott@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:19:46 by etachott          #+#    #+#             */
-/*   Updated: 2022/11/17 14:47:40 by etachott         ###   ########.fr       */
+/*   Updated: 2022/11/17 14:57:22 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	check_extremes(char *argv[])
+{
+	int	max;
+	int	min;
+	int	index;
+
+	max = 2147483647;
+	min = -2147483648;
+	index = 0;
+	while (argv[index])
+	{
+		if (ft_atoi(argv[index]) > max || ft_atoi(argv[index]) < min)
+			return (0);
+		index++;
+	}
+	return (1);
+}
 
 int	check_doubles(char *argv[])
 {
@@ -41,6 +59,8 @@ int	validate_input(char *argv[])
 
 	index = 1;
 	if (!check_doubles(argv))
+		return (0);
+	if (!check_extremes(argv))
 		return (0);
 	while (argv[index])
 	{
